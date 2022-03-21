50fbef27a2aedda7f08090484d349ae08466c90d
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-21 21:27:45 +0100

    Après la mise a jour de script shell, 3 commandes en one shot

--- Run.sh
+++ Run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python -m py.test -v -s /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_IDenBoucle.py
+python -m py.test -v -s /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
+sleep 3
+python -m py.test -v -s /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_modif_profil.py 
+sleep 3
+python -m py.test -v -s /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
