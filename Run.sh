62ced7663bd4aeeceb3e3001bcab3443c8ba9989
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-22 10:39:19 +0100

    maj de script shell

--- Run.sh
+++ Run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 python -m py.test --alluredir=Reports/my_allure_report_sh /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
-sleep 3
-allure serve Reports/my_allure_report_sh    
