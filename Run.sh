cef154b96d112848007432beabada560efb69b1e
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-22 11:45:04 +0100

    maj de script shell + ajout dossier allure

--- Run.sh
+++ Run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m py.test --alluredir=Reports/my_allure_report_sh /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
+python -m py.test /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
