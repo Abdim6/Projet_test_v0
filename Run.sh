e89f94dee942ef6c944b45d757b55ce590c2800c
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-23 11:06:48 +0100

    deuxieme jet de nettoyage de code version 0

--- Run.sh
+++ Run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-python -m py.test /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_modif_profil.py 
+python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py 
+sleep 3
+python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_modif_profil.py 
+sleep 3
+python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py c
+sleep 3
+allure serve Reports/my_allure_report   
 
