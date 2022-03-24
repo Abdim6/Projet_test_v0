1df9922ba4a879e603201271935f4e41314a713b
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-24 12:00:59 +0100

    maj du fichier locator avec les By.correspondant

--- PageObjects/Locator.py
+++ PageObjects/Locator.py
@@ -8,23 +8,23 @@ from selenium.webdriver.common.by import By
 class Locators():
     #--- Page Home Page ---
     # moncompte_btn = (By.CSS_SELECTOR, "---")
-    moncompte_btn_CSS = "#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(5) > button"
-    Chaines_btn_Class =""
-    recherche_btn_CSS = "#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(4) > a"
-    consent_accepter_Class = "sc-1esye45-2.dVskuE.sc-1p0v3oj-0"
-    accepter_btnTCF_CSS = "body > div:nth-child(12) > aside > div > div.suptlw-3.dTtZno > form > div.s9pqpz-4.eGAKcX.sc-18bqyzn-0.gXHDvG > div > button.sc-1esye45-2.dVskuE.sc-1veuio6-0.dktssj.s9pqpz-2.hEklsz.is-primary > span"
-    HomeBtn_CSS = "#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(3)"
-    btnListe_chaines_Class = "sc-1ajxxj-0.sc-1c6u83a-3.iUrMAG"
-    liste_chaines_class = "b8xld8-1"
-    objetHomePage_CSS = "#main > div > section:nth-child(3) > div.y84eg7-0.sc-8pi7if-0.LuxLU > h1"
-    moncompte_btn_xml = '//*[@id="__brk"]/div/div[2]/div[2]/div/header/div/div/nav/div[1]/ul/li[5]/button'
-    mesinfo_btn = "#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > main > aside > nav > ul > li:nth-child(3) > a > span"
-    mesinfo_btn_xml = '//*[@id="__brk"]/div/div[2]/div[2]/div/main/aside/nav/ul/li[3]/a'
-    donneesperso_email = "email"
-    AjoutFavoris_btn_CSS = "#page_62209efac81c31\.51376685--4f8b12bd-ed93-4057-80fa-211a289dc3a4 > div.qd8avx-0.jwKxkB > div.nei5bm-2.cNRBFu > div.nei5bm-9.fEDJNC > button"
-    AjoutFavoris_btn_Class = "oxwymj-0.jtrbaw.sc-1qpa8kx-4.eOyIfE.is-secondary.is-responsive"
-    AjoutFavoris_btn_Class_2 = "f1uzip-0.hUbPBT"
-    AjoutFavoris_btn_Xpath = '//*[@id="page_6230ac5c387d43.50469081--6a469067-d4c3-4e3f-a32f-46167d79122d"]/div[2]/div[2]/div[1]/button'
+    moncompte_btn_CSS = (By.CSS_SELECTOR,"#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(5) > button")
+    Chaines_btn_Class =(By.CLASS_NAME,"")
+    recherche_btn_CSS = (By.CSS_SELECTOR,"#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(4) > a")
+    consent_accepter_Class = (By.CLASS_NAME,"sc-1esye45-2.dVskuE.sc-1p0v3oj-0")
+    accepter_btnTCF_CSS = (By.CSS_SELECTOR,"body > div:nth-child(12) > aside > div > div.suptlw-3.dTtZno > form > div.s9pqpz-4.eGAKcX.sc-18bqyzn-0.gXHDvG > div > button.sc-1esye45-2.dVskuE.sc-1veuio6-0.dktssj.s9pqpz-2.hEklsz.is-primary > span")
+    HomeBtn_CSS = (By.CSS_SELECTOR,"#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > header > div > div > nav > div.m4392j-1.imWKGD > ul > li:nth-child(3)")
+    btnListe_chaines_Class = (By.CLASS_NAME,"sc-1ajxxj-0.sc-1c6u83a-3.iUrMAG")
+    liste_chaines_class = (By.CLASS_NAME,"b8xld8-1")
+    objetHomePage_CSS = (By.CSS_SELECTOR,"#main > div > section:nth-child(3) > div.y84eg7-0.sc-8pi7if-0.LuxLU > h1")
+    moncompte_btn_xml = (By.XPATH,'//*[@id="__brk"]/div/div[2]/div[2]/div/header/div/div/nav/div[1]/ul/li[5]/button')
+    mesinfo_btn = (By.CSS_SELECTOR,"#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > main > aside > nav > ul > li:nth-child(3) > a > span")
+    mesinfo_btn_xml = (By.XPATH,'//*[@id="__brk"]/div/div[2]/div[2]/div/main/aside/nav/ul/li[3]/a')
+    donneesperso_email = (By.ID,"email")
+    AjoutFavoris_btn_CSS = (By.CSS_SELECTOR,"#page_62209efac81c31\.51376685--4f8b12bd-ed93-4057-80fa-211a289dc3a4 > div.qd8avx-0.jwKxkB > div.nei5bm-2.cNRBFu > div.nei5bm-9.fEDJNC > button")
+    AjoutFavoris_btn_Class = (By.CLASS_NAME,"oxwymj-0.jtrbaw.sc-1qpa8kx-4.eOyIfE.is-secondary.is-responsive")
+    AjoutFavoris_btn_Class_2 = (By.CLASS_NAME,"f1uzip-0.hUbPBT")
+    AjoutFavoris_btn_Xpath = (By.XPATH,'//*[@id="page_6230ac5c387d43.50469081--6a469067-d4c3-4e3f-a32f-46167d79122d"]/div[2]/div[2]/div[1]/button')
 
     #--- Page Mon Compte ---
     btn_gererInfos_CSS = "#__brk > div > div.n913wo-5.cuyMWW > div.n913wo-4.fCmDdf > div > main > div > div.r1if2e-1.bqqlfm.sc-1eq6ctx-0.kabTKW > div:nth-child(1) > article > div.gvmwfe-0.o6a45d-0.kfijhj > a"
--- Run.sh
+++ Run.sh
@@ -3,7 +3,7 @@ python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Docum
 sleep 3
 python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_modif_profil.py 
 sleep 3
-python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_homePage_chaines.py c
+python -m py.test --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_IDenBoucle.py
 sleep 3
 allure serve Reports/my_allure_report   
 
