215d69a27d62579eb20071579dde2257f273c1cc
Abdim6 <abdi.bilehm6@gmail.com>
2022-03-28 21:45:42 +0200

    update monCompte file using action_OnElem class

--- PageObjects/MonCompte.py
+++ PageObjects/MonCompte.py
@@ -8,27 +8,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from PageObjects.Locator import Locators
+from TestCases.conftest2 import action_OnElem
 
 class MonCompte:
     def __init__(self, driver):
-        self.driver = driver
+        # self.driver = driver
+        self.obj = action_OnElem(driver)
     
     def clickgenerInfo(self):
-        self.driver.find_element(Locators.btn_gererInfos_CSS).click()
+        # self.driver.find_element(Locators.btn_gererInfos_CSS).click()
+        self.obj.click(Locators.btn_gererInfos_CSS)
 
     def clickModifier(self):
-        self.driver.find_element(Locators.btn_modifier_CSS).click()
+        # self.driver.find_element(Locators.btn_modifier_CSS).click()
+        self.obj.click(Locators.btn_modifier_CSS)
 
     def setPrenom(self, prenom):
-        self.driver.find_element(Locators.textbox_firstname_id).clear()
-        time.sleep(1)
-        self.driver.find_element(Locators.textbox_firstname_id).send_keys(prenom)
+        # self.driver.find_element(Locators.textbox_firstname_id).clear()
+        # time.sleep(1)
+        # self.driver.find_element(Locators.textbox_firstname_id).send_keys(prenom)
+        self.obj.enter_text(Locators.textbox_firstname_id,prenom)
 
     def setNom(self, nom):
-        self.driver.find_element(Locators.textbox_lastname_id).clear()
-        self.driver.find_element(Locators.textbox_lastname_id).send_keys(nom)
+        # self.driver.find_element(Locators.textbox_lastname_id).clear()
+        # self.driver.find_element().send_keys(nom)
+        self.obj.enter_text(Locators.textbox_lastname_id,nom)
 
     def clickValider(self):
-        self.driver.find_element(Locators.btn_valider_CSS).click()
-
+        # self.driver.find_element(Locators.btn_valider_CSS).click()
+        self.obj.click(Locators.btn_valider_CSS)
   
