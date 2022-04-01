e8b56aedb6a71925aecefffbc543e7cefdb23c51
Abdim6 <abdi.bilehm6@gmail.com>
2022-04-01 16:21:12 +0200

    refacto common_action avec facto self.wait + ignore tous les fichier pycache

--- PageObjects/common_Actions.py
+++ PageObjects/common_Actions.py
@@ -44,41 +44,45 @@ class common_Actions():
     
     # this function asserts comparison of a web element's text with passed in text.
     def assert_element_text(self, by_locator, element_text):
-        web_element=WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        web_element=self.wait.until(EC.visibility_of_element_located(by_locator))
         assert web_element.text == element_text
 
     # this function performs text entry of the passed in text, in a web element whose locator is passed to it.
     def enter_text(self, by_locator, text):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
+        return self.wait.until(EC.visibility_of_element_located(by_locator)).send_keys(text)
 
     # this function checks if the web element whose locator has been passed to it, is enabled or not and returns
     # web element if it is enabled.
     def is_enabled(self, by_locator):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        return self.wait.until(EC.visibility_of_element_located(by_locator))
 
     # this function checks if the web element whose locator has been passed to it, is visible or not and returns
     # true or false depending upon its visibility.
     def is_visible(self,by_locator):
-        element=WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        element= self.wait.until(EC.visibility_of_element_located(by_locator))
         return bool(element)
     
     # this function moves the mouse pointer over a web element whose locator has been passed to it.
     def hover_to(self, by_locator):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        element = self.wait.until(EC.visibility_of_element_located(by_locator))
         ActionChains(self.driver).move_to_element(element).perform()
 
     def select_InList(self,by_locator,index):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        element = self.wait.until(EC.visibility_of_element_located(by_locator))
         drp = Select(element)
         drp.select_by_index(index)
 
     def get_value(self,by_locator):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        element = self.wait.until(EC.visibility_of_element_located(by_locator))
         return element.get_property("value")
 
     "cette fonction retourne la liste de tous elements"
     def get_list_elements(self,by_locator):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located(by_locator))
+        return self.wait.until(EC.visibility_of_all_elements_located(by_locator))
         # self.driver.find_element(by_locator)
         # import pdb; pdb.set_trace()
         # self.driver.find_elements(By.CLASS_NAME,"b8xld8-1")
+
+
+
+        "Attention faut utiliser des fonction à la place de la classe"
