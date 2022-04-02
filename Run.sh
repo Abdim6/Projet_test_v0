d8e28339e72efbb382e34bd0e92dce95ec8d01f9
Abdim6 <abdi.bilehm6@gmail.com>
2022-04-02 05:24:10 +0200

    mise en commentaire la commande rapport allure dans le .sh

--- Run.sh
+++ Run.sh
@@ -5,7 +5,8 @@ pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Seleni
 sleep 3
 pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_IDenBoucle.py
 sleep 3
-allure serve Reports/my_allure_report   
+
+#allure serve Reports/my_allure_report   
 
 # "commande avec allure"
 # pytest --alluredir TestCases/test_modif_profil.py
