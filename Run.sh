ad168f9252693c722524767387b2a7bbcd926700
Abdim6 <abdi.bilehm6@gmail.com>
2022-04-12 13:55:40 +0200

    Script shell maj

--- Run.sh
+++ Run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_001_Connexion.py --capture=tee-sys
-sleep 3
-pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_002_ModifierDonneesUser.py --capture=tee-sys
-sleep 3
-pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_003_HomePage.py --capture=tee-sys
-sleep 3
+# sleep 3
+# pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_002_ModifierDonneesUser.py --capture=tee-sys
+# sleep 3
+# pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_003_HomePage.py --capture=tee-sys
+# sleep 3
 
 # pytest /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_001_Connexion.py --capture=tee-sys
 # sleep 3
@@ -13,7 +13,7 @@ sleep 3
 # pytest /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_003_HomePage.py --capture=tee-sys
 # sleep 3
 
-allure serve Reports/my_allure_report   
+# allure serve Reports/my_allure_report   
 
 # "commande avec allure"
 # pytest --alluredir TestCases/test_modif_profil.py
