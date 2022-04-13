8ab077eb6a30a9a4fb60bd2888c78b5de05f0763
Abdim6 <abdi.bilehm6@gmail.com>
2022-04-13 17:29:06 +0200

    création dossier allure-results...V6-- après avoir trouvé la solution pour le allure report

--- Run.sh
+++ Run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pytest --alluredir=allure-results /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_001_Connexion.py --capture=tee-sys
 sleep 3
-pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_002_ModifierDonneesUser.py --capture=tee-sys
+pytest --alluredir=allure-results /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_002_ModifierDonneesUser.py --capture=tee-sys
 sleep 3
-pytest --alluredir=Reports/my_allure_report /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_003_HomePage.py --capture=tee-sys
+pytest --alluredir=allure-results /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_003_HomePage.py --capture=tee-sys
 sleep 3
 
 # pytest /Users/abdi.bileh17/Documents/Selenium/Projet_test_v0/TestCases/test_001_Connexion.py --capture=tee-sys
